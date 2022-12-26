2fc9857bd6cd98b82c84a46a9beb9e2e68e0c092
David <davidbalyan9@gmail.com>
2022-12-26 21:20:48 +0200

    fixed playback controls and added new bg

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,19 @@ import React from "react";
 import Sound from "./Sound";
 
 const Home = () => {
-	const prefix = "https://cdn.jsdelivr.net/gh/davidbalian/isolate-sounds/";
+  const prefix = "https://cdn.jsdelivr.net/gh/davidbalian/isolate-sounds/";
 
-	return (
-		<div>
-			<div className='sounds'>
-				<Sound src={prefix + "rain-light.wav"} name='rain' />
-				<Sound src={prefix + "fire.wav"} name='fire' />
-				<Sound src={prefix + "birds.wav"} name='birds' />
-				<Sound src={prefix + "waves-heavy.wav"} name='heavy waves' />
-				<Sound src={prefix + "cafe.wav"} name='cafe' />
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="sounds">
+        <Sound src={prefix + "rain-light.wav"} name="rain" />
+        <Sound src={prefix + "fire.wav"} name="fire" />
+        <Sound src={prefix + "birds.wav"} name="birds" />
+        <Sound src={prefix + "waves-heavy.wav"} name="heavy waves" />
+        <Sound src={prefix + "cafe.wav"} name="cafe" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Sound.js
+++ src/components/Sound.js
@@ -1,42 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Howl } from "howler";
-import PlayCircleFilledRoundedIcon from '@mui/icons-material/PlayCircleFilledRounded';
-import PauseCircleFilledRoundedIcon from '@mui/icons-material/PauseCircleFilledRounded';
+import PlayCircleFilledRoundedIcon from "@mui/icons-material/PlayCircleFilledRounded";
+import PauseCircleFilledRoundedIcon from "@mui/icons-material/PauseCircleFilledRounded";
 
 const Sound = ({ src, name }) => {
-	const [playing, setPlaying] = useState(0);
+  let id1;
+  const sound = new Howl({
+    src: src,
+    html5: true,
+    volume: 0.75,
+  });
 
-	const sound = new Howl({
-		src: src,
-		html5: true,
-		volume: .75	
-	});
+  const handleVolumeChange = (e) => {
+    sound.volume(parseInt(e.target.value, 10) / 100);
+  };
 
-	const handleVolumeChange = (e) => {
-		sound.volume(parseInt(e.target.value, 10) / 100);
-	}
+  return (
+    <div className="sound">
+      <p className="sound-name">{name}</p>
+      <div className="controls">
+        <PlayCircleFilledRoundedIcon
+          className="play-pause-btn"
+          onClick={() => {
+            sound.play();
+          }}
+        />
 
-	const togglePlay = () => {
-		if (sound.playing()) {
-			sound.pause();
-		} else {
-			sound.play();
-		}
-	}
-
-	return (
-		<div className='sound'>
-			<p className='sound-name'>{name}</p>
-			<div className='play-pause-container' onClick={() => {
-				togglePlay();
-				playing ? setPlaying(0) : setPlaying(1);}}>
-				{playing ? <PauseCircleFilledRoundedIcon /> : <PlayCircleFilledRoundedIcon />}
-			</div>
-			<div className='volume-controls'>
-				<input type="range" className="volume-slider" max="100" defaultValue="75" onChange={handleVolumeChange}/>
-			</div>
-		</div>
-	);
+        <div className="volume-controls">
+          <input
+            type="range"
+            className="volume-slider"
+            max="100"
+            defaultValue="75"
+            onChange={handleVolumeChange}
+          />
+        </div>
+        <PauseCircleFilledRoundedIcon
+          className="play-pause-btn"
+          onClick={() => {
+            sound.pause();
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Sound;
