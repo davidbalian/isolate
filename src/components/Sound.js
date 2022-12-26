abdc4678f7f4270b46af3a69878cb20bbcc563a3
David <davidbalyan9@gmail.com>
2022-12-26 04:25:36 +0200

    added new volume and play controls

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -1,74 +1,39 @@
-import React, { useEffect, useState } from "react";
-import { Howl, Howler } from "howler";
+import React, { useState } from "react";
+import { Howl } from "howler";
+import PlayCircleFilledRoundedIcon from '@mui/icons-material/PlayCircleFilledRounded';
+import PauseCircleFilledRoundedIcon from '@mui/icons-material/PauseCircleFilledRounded';
 
 const Sound = ({ src, name }) => {
-	const [volume, setVolume] = useState(0.75);
+	const [playing, setPlaying] = useState(0);
 
 	const sound = new Howl({
 		src: src,
-		html5: true
+		html5: true,
+		volume: .75	
 	});
 
-	useEffect(() => {
-		Howler.volume(volume);
-	}, [volume, setVolume]);
+	const handleVolumeChange = (e) => {
+		sound.volume(parseInt(e.target.value, 10) / 100);
+	}
+
+	const togglePlay = () => {
+		if (sound.playing()) {
+			sound.pause();
+		} else {
+			sound.play();
+		}
+	}
 
 	return (
 		<div className='sound'>
 			<p className='sound-name'>{name}</p>
-			<div className='play-pause-container'>
-				<button
-					className='play-pause play'
-					onClick={() => {
-						sound.play();
-					}}
-				>
-					play
-				</button>
-				<button
-					className='play-pause pause'
-					onClick={() => {
-						sound.pause();
-					}}
-				>
-					pause
-				</button>
+			<div className='play-pause-container' onClick={() => {
+				togglePlay();
+				playing ? setPlaying(0) : setPlaying(1);}}>
+				{playing ? <PauseCircleFilledRoundedIcon /> : <PlayCircleFilledRoundedIcon />}
 			</div>
 			<div className='volume-controls'>
-				<div className='volume-graph'>
-					<div
-						className={`volume-bar quarter ${
-							volume === 0.25 || volume === 0.5 || volume === 0.75 || volume === 1
-								? "active"
-								: null
-						}`}
-						onClick={() => {
-							setVolume(0.25);
-						}}
-					></div>
-					<div
-						className={`volume-bar half ${
-							volume === 0.5 || volume === 0.75 || volume === 1 ? "active" : null
-						}`}
-						onClick={() => {
-							setVolume(0.5);
-						}}
-					></div>
-					<div
-						className={`volume-bar three-quarters ${
-							volume === 0.75 || volume === 1 ? "active" : null
-						}`}
-						onClick={() => {
-							setVolume(0.75);
-						}}
-					></div>
-					<div
-						className={`volume-bar full ${volume === 1 ? "active" : null}`}
-						onClick={() => {
-							setVolume(1);
-						}}
-					></div>
-				</div>
+				<input type="range" className="volume-slider" max="100" defaultValue="75" onChange={handleVolumeChange}/>
 			</div>
 		</div>
 	);
