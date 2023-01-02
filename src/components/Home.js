9414d390ed32a5436f56b5596dc29577097a422d
David <davidbalyan9@gmail.com>
2023-01-02 16:00:10 +0200

    changed font

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,21 @@ const Home = () => {
 
   return (
     <div className="home">
-      <div className="sounds">
-        <Sound src={prefix + "brown-noise.mp3"} name="brown noise" />
-        <Sound src={prefix + "rain-light-long.mp3"} name="rain" />
-        <Sound src={prefix + "fire-long.mp3"} name="fire" />
-        <Sound src={prefix + "birds-long.mp3"} name="birds" />
-        <Sound src={prefix + "heavy-waves-long.mp3"} name="heavy waves" />
-        <Sound src={prefix + "cafe-long.mp3"} name="cafe" />
+      <div className="main-functionality">
+        <div className="sounds">
+          <Sound src={prefix + "brown-noise.mp3"} name="brown noise" />
+          <Sound src={prefix + "rain-light-long.mp3"} name="rain" />
+          <Sound src={prefix + "fire-long.mp3"} name="fire" />
+          <Sound src={prefix + "birds-long.mp3"} name="birds" />
+          <Sound src={prefix + "heavy-waves-long.mp3"} name="heavy waves" />
+          <Sound src={prefix + "cafe-long.mp3"} name="cafe" />
+        </div>
+        <div className="text-container">
+          <p className="text">
+            Click on the play button to play. Adjust the volume using the volume
+            slider next to it. Click on the pause button to pause.
+          </p>
+        </div>
       </div>
     </div>
   );
